c09437bb8eeff4b260bbcd8953eb95dda2fd3e72
tanveerhasan <tanveer.cse01@gmail.com>
2021-02-11 01:23:55 +0600

    modify service

--- app/src/main/java/com/example/backgroundtask/MainActivity.java
+++ app/src/main/java/com/example/backgroundtask/MainActivity.java
@@ -20,8 +20,7 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String NOTIFICATION_CHANNEL_ID = "10001" ;
-    private final static String default_notification_channel_id = "default" ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,38 +37,18 @@ public class MainActivity extends AppCompatActivity {
         intent.setAction("BackgroundProcess");
 
         //Set the repeated Task
+
+
+
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, 0, 1, pendingIntent);
 
-        scheduleNotification(getNotification( "5 second delay" ) , 5000 ) ;
-
-
+        finish();
 
 
     }
 
-    private void scheduleNotification (Notification notification , int delay) {
-        Intent notificationIntent = new Intent( this, myBackgroundProcess. class ) ;
-        notificationIntent.putExtra(myBackgroundProcess. NOTIFICATION_ID , 1 ) ;
-        notificationIntent.putExtra(myBackgroundProcess. NOTIFICATION , notification) ;
-        PendingIntent pendingIntent = PendingIntent. getBroadcast ( this, 0 , notificationIntent , PendingIntent. FLAG_UPDATE_CURRENT ) ;
-        long futureInMillis = SystemClock. elapsedRealtime () + delay ;
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context. ALARM_SERVICE ) ;
-        assert alarmManager != null;
-        alarmManager.set(AlarmManager. ELAPSED_REALTIME_WAKEUP , futureInMillis , pendingIntent) ;
-    }
-
-
-    private Notification getNotification (String content) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder( this, default_notification_channel_id ) ;
-        builder.setContentTitle( "Scheduled Notification" ) ;
-        builder.setContentText(content) ;
-        builder.setSmallIcon(R.drawable. ic_launcher_foreground ) ;
-        builder.setAutoCancel( true ) ;
-        builder.setChannelId( NOTIFICATION_CHANNEL_ID ) ;
-        return builder.build() ;
-    }
 
 
 }
--- app/src/main/java/com/example/backgroundtask/myBackgroundProcess.java
+++ app/src/main/java/com/example/backgroundtask/myBackgroundProcess.java
@@ -16,12 +16,10 @@ import androidx.core.app.NotificationCompat;
 
 import java.util.Calendar;
 
-import static com.example.backgroundtask.MainActivity.NOTIFICATION_CHANNEL_ID;
 
 public class myBackgroundProcess extends BroadcastReceiver {
 
-    public static String NOTIFICATION_ID = "notification-id" ;
-    public static String NOTIFICATION = "notification" ;
+
 
 
     @Override
@@ -31,19 +29,6 @@ public class myBackgroundProcess extends BroadcastReceiver {
         ringtone.play();
 
         Toast.makeText(context, "This is my background process: \n" + Calendar.getInstance().getTime().toString(), Toast.LENGTH_LONG).show();
-
-
-     NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context. NOTIFICATION_SERVICE ) ;
-        Notification notification = intent.getParcelableExtra( NOTIFICATION ) ;
-        if (android.os.Build.VERSION. SDK_INT >= android.os.Build.VERSION_CODES. O ) {
-            int importance = NotificationManager. IMPORTANCE_HIGH ;
-            NotificationChannel notificationChannel = new NotificationChannel( NOTIFICATION_CHANNEL_ID , "NOTIFICATION_CHANNEL_NAME" , importance) ;
-            assert notificationManager != null;
-            notificationManager.createNotificationChannel(notificationChannel) ;
-        }
-        int id = intent.getIntExtra( NOTIFICATION_ID , 0 ) ;
-        assert notificationManager != null;
-        notificationManager.notify(id , notification) ;
 
 
         SystemClock.sleep(0);
